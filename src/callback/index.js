b227ed61d0c4862f4307193045ec611cf77509e7
ramsceoSkull <ramsesrame21@gmail.com>
2021-08-11 19:40:33 -0500

    Primer avance

---
+++ src/callback/index.js
@@ -0,0 +1,25 @@
+// Callback 1
+    function sum(num1, num2) {
+        return num1 + num2;
+    }
+
+    function calc(num1, num2, callback) {
+        return callback(num1, num2);
+    }
+
+    console.log(calc(6, 2, sum));
+
+// Callback 2
+    function date(callback) {
+        console.log(new Date);  // Sirve para acceder a la fecha de este momento
+        setTimeout(function () {
+            let date = new Date;
+            callback(date);
+        }, 3000);
+    }
+
+    function printDate(dateNow) {
+        console.log(dateNow);
+    }
+
+    date(printDate);
